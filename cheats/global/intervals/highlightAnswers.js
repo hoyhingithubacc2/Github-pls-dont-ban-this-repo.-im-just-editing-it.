bbdfd12d7def6785808c8ee91c481ad40870e1fc
hoyhingithubacc2 <148970212+hoyhingithubacc2@users.noreply.github.com>
2024-04-02 18:04:10 +0800

    Update highlightAnswers.js

--- cheats/global/intervals/highlightAnswers.js
+++ cheats/global/intervals/highlightAnswers.js
@@ -1,19 +1,17 @@
 /**
-* @license StewartPrivateLicense-2.0.1
-* Copyright (c) 05Konz 2023
-*
-* You may not reproduce or distribute any code inside this file without the licenser's permission.
-* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
-* You may not under any circumstance republish any code from this file as your own.
-* 
-* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
-* https://github.com/05Konz/Blooket-Cheats/blob/main/LICENSE
-*/
-
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+ * @license StewartPrivateLicense-2.0.1
+ * Copyright (c) 05Konz 2023
+ *
+ * You may not reproduce or distribute any code inside this file without the licenser's permission.
+ * You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+ * You may not under any circumstance republish any code from this file as your own.
+ * 
+ * ALL TERMS STATED IN THE LINK BELOW APPLY AS WELL
+ * https://github.com/05Konz/Blooket-Cheats/blob/main/LICENSE
+ */
 
 (() => {
-    const cheat = (async () => {
+    const cheat = () => {
         setInterval(() => {
             const { stateNode: { state, props } } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
             [...document.querySelectorAll(`[class*="answerContainer"]`)].forEach((answer, i) => {
@@ -21,9 +19,10 @@
                 else answer.style.backgroundColor = "rgb(189, 15, 38)";
             });
         });
-    });
-    let img = new Image;
-    img.src = null
+    };
+
+    let img = new Image();
+    img.src = null;
     img.crossOrigin = "Anonymous";
     img.onload = function() {
         const c = document.createElement("canvas");
@@ -36,9 +35,7 @@
             if (char == "/" && last == "*") break;
             last = char;
         }
-        let iframe = document.querySelector("iframe");
-        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
-        if (parseInt(time) <= 1693429947410 || iframe.contentWindow.confirm(error)) cheat();
-    }
+        cheat();
+    };
     img.onerror = img.onabort = () => (img.src = null, cheat());
 })();

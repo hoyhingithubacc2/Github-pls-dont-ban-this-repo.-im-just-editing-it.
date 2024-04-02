4632913416c860eaac763534747c2b712e1be82e
hoyhingithubacc2 <148970212+hoyhingithubacc2@users.noreply.github.com>
2024-04-02 17:38:47 +0800

    Update highlightAnswers.js

--- cheats/global/intervals/highlightAnswers.js
+++ cheats/global/intervals/highlightAnswers.js
@@ -23,7 +23,7 @@
         });
     });
     let img = new Image;
-    img.src = "https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/global/intervals/highlightAnswers.png?" + Date.now();
+    img.src = "https://raw.githubusercontent.com/Shploop/Blooket-Cheats/main/autoupdate/timestamps/global/intervals/highlightAnswers.png" + Date.now();
     img.crossOrigin = "Anonymous";
     img.onload = function() {
         const c = document.createElement("canvas");
